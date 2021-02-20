a745416865885943fd79bad5d6f0915ae13e7b4a
realisticattorney <g.axl.aquila@gmail.com>
2021-02-20 01:22:02 -0300

    finish

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,13 +1,13 @@
 module EventsHelper
   def enrollment_button(event)
     return unless current_user
-    
-      if event.host == current_user
-        link_to 'You created this event. View analytics', event_path(event)
-      elsif event.enrollments.where(user: current_user).any?
-        link_to 'You enrolled this event', event_path(event)
-      else
-        link_to 'Free', new_enrollment_path(event), class: 'btn btn-success'
-      end
+
+    if event.host == current_user
+      link_to 'You created this event. View analytics', event_path(event)
+    elsif event.enrollments.where(user: current_user).any?
+      link_to 'You enrolled this event', event_path(event)
+    else
+      link_to 'Free', new_enrollment_path(event), class: 'btn btn-success'
+    end
   end
 end
